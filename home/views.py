8649ac4e99d044ca1e389b507e3316993cb1325c
Luksh <hotmale_luksh@hotmail.com>
2022-05-08 14:58:03 +0545

    login

--- home/views.py
+++ home/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
+
 def index(request):
-    render(request, 'index.html')
+    if request.user.is_anonymous:
+        return redirect('/login')
+    return render(request, 'index.html')
+
 
 def login(request):
-    render(request, 'logout.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username='username', password='password')
+        if user is not None:
+            return redirect('/')
+        else:
+            return render(request, 'login.html')
+    return render(request, 'login.html')
+
 
 def logout(request):
-    render(request, 'index.html')
+    return render(request, 'index.html')
